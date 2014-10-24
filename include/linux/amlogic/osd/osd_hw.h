036e4412c141a8b12685b91c36ac4639bf39d670
Daniel Drake <drake@endlessm.com>
2014-10-24 10:27:05 -0600

    linux-3.10-include-linux-amlogic-osd-osd_hw.patch

--- include/linux/amlogic/osd/osd_hw.h
+++ include/linux/amlogic/osd/osd_hw.h
@@ -240,6 +240,7 @@ extern s32  osd_wait_vsync_event(void);
 #if defined(CONFIG_FB_OSD2_CURSOR)
 extern void osd_cursor_hw(s16 x, s16 y, s16 xstart, s16 ystart, u32 osd_w, u32 osd_h, int index);
 #endif
+extern void osddev_copy_data_tocursor_hw(u32 cursor_mem_paddr, aml_hwc_addr_t *hwc_mem);
 extern void osd_suspend_hw(void);
 extern void osd_resume_hw(void);
 extern void osd_init_hw(u32  logo_loaded);
