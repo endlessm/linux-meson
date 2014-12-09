baec1a1caadc177707756c3246e9df0a67720b6b
Jasper St. Pierre <jstpierre@mecheye.net>
2014-12-09 12:09:09 -0800

    drm/meson: Split mode enumeration out to a different file
    And use the DRM mode name rather than accurate timing information to
    identify standard DRM modes, as it's much simpler to recognize and
    doesn't have typos.

---
+++ drivers/gpu/drm/meson/meson_modes.c
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2014 Endless Mobile
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * Written by:
+ *     Jasper St. Pierre <jstpierre@mecheye.net>
+ */
+
+#include "meson_modes.h"
+
+/* XXX: Replace this with our own HDMI driver eventually? */
+static const struct {
+	const char *drm_mode_name;
+	vmode_t vmode;
+} supported_modes[] = {
+	{ "640x480",   VMODE_VGA },
+	{ "720x480",   VMODE_480P },
+	{ "1280x720",  VMODE_720P },
+	{ "1920x1080", VMODE_1080P },
+};
+
+vmode_t drm_mode_to_vmode(const struct drm_display_mode *mode)
+{
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(supported_modes); i++) {
+		if (strcmp(mode->name, supported_modes[i].drm_mode_name) == 0)
+			return supported_modes[i].vmode;
+	}
+
+	return -1;
+}
