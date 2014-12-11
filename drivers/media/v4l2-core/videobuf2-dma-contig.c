cdd12302322e672a2449e13b3b1e261b930b80a7
Daniel Drake <drake@endlessm.com>
2014-12-11 12:01:52 -0800

    videobuf2: fix for new dmabuf API
    The drm backport updated the dmabuf API, so this caller needs to be
    fixed.

--- drivers/media/v4l2-core/videobuf2-dma-contig.c
+++ drivers/media/v4l2-core/videobuf2-dma-contig.c
@@ -404,7 +404,7 @@ static struct dma_buf *vb2_dc_get_dmabuf(void *buf_priv)
 	if (WARN_ON(!buf->sgt_base))
 		return NULL;
 
-	dbuf = dma_buf_export(buf, &vb2_dc_dmabuf_ops, buf->size, 0);
+	dbuf = dma_buf_export(buf, &vb2_dc_dmabuf_ops, buf->size, 0, NULL);
 	if (IS_ERR(dbuf))
 		return NULL;
 
