6939c2fa2f17d1d9ddadb142adf797c75e9cf80c
Daniel Drake <drake@endlessm.com>
2015-03-26 08:37:39 -0600

    Update to Mali/UMP/umplock r5p1-01rel0
    Pristine from ARM tarball.
    UMP includes put in place under ump/ (which originate from another part
    of the tarball).

--- drivers/amlogic/gpu/ump/include/ump/ump_kernel_interface_ref_drv.h
+++ drivers/amlogic/gpu/ump/include/ump/ump_kernel_interface_ref_drv.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010, 2013 ARM Limited. All rights reserved.
+ * Copyright (C) 2010, 2013, 2015 ARM Limited. All rights reserved.
  * 
  * This program is free software and is provided to you under the terms of the GNU General Public License version 2
  * as published by the Free Software Foundation, and any use by you of this program is subject to the terms of such GNU licence.
@@ -23,7 +23,6 @@ extern "C" {
 
 /** Turn specified physical memory into UMP memory. */
 UMP_KERNEL_API_EXPORT ump_dd_handle ump_dd_handle_create_from_phys_blocks(ump_dd_physical_block *blocks, unsigned long num_blocks);
-UMP_KERNEL_API_EXPORT ump_dd_handle ump_dd_handle_create_from_phys_blocks2(ump_dd_physical_block *blocks, unsigned long num_blocks, int is_cached);
 
 #ifdef __cplusplus
 }
