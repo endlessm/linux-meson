6db0d0162b68596ab666bb2bd5f238f8938c8b42
Carlo Caione <carlo@endlessm.com>
2015-04-29 16:55:52 +0200

    Revert "Mali: avoid powering down an always-on domain"
    This reverts commit 84a590fff707f079fcfab4b34a43a502da2dd18a.

--- drivers/amlogic/gpu/mali/common/mali_pmu.c
+++ drivers/amlogic/gpu/mali/common/mali_pmu.c
@@ -145,10 +145,6 @@ _mali_osk_errcode_t mali_pmu_power_down(struct mali_pmu_core *pmu, u32 mask)
 
 	if (0 == mask || 0 == ((~stat) & mask)) return _MALI_OSK_ERR_OK;
 
-	/* This power domain never reports itself as off, so drop all requests
-	 * that only ask to turn it off, since they would fail. */
-	if (mask == 8) return _MALI_OSK_ERR_OK;
-
 	mali_hw_core_register_write(&pmu->hw_core,
 				    PMU_REG_ADDR_MGMT_POWER_DOWN, mask);
 
