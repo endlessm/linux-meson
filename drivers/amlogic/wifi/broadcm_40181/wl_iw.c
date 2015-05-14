c19039007ba6526c07cfc7bba5f4d3c95fb9c23d
Daniel Drake <drake@endlessm.com>
2015-05-14 08:00:55 -0600

    broadcom_40181: fix modular build
    kernel_thread is not exported, and the _MODULE option suffix is used
    internally in Kbuild for modules.
    
    [endlessm/eos-shell#5026]

--- drivers/amlogic/wifi/broadcm_40181/wl_iw.c
+++ drivers/amlogic/wifi/broadcm_40181/wl_iw.c
@@ -3758,6 +3758,7 @@ int
 wl_iw_attach(struct net_device *dev, void * dhdp)
 {
 	iscan_info_t *iscan = NULL;
+	struct task_struct *task;
 
 	if (!dev)
 		return 0;
@@ -3781,7 +3782,8 @@ wl_iw_attach(struct net_device *dev, void * dhdp)
 
 	sema_init(&iscan->sysioc_sem, 0);
 	init_completion(&iscan->sysioc_exited);
-	iscan->sysioc_pid = kernel_thread(_iscan_sysioc_thread, iscan, 0);
+	task = kthread_run(_iscan_sysioc_thread, iscan, "bcm40181_sysioc");
+	iscan->sysioc_pid = task->pid;
 	if (iscan->sysioc_pid < 0)
 		return -ENOMEM;
 	return 0;
