25bbdead5817136f30187940e7e1ac3c2f96d485
João Paulo Rechi Vita <jprvita@endlessm.com>
2015-06-24 11:56:06 -0400

    drm/meson: Add cache synchronization primitives
    Now that libMali supports DMA-BUF we want to get rid from UMP in
    meson-drm. For that to be possible we need to provide cache
    synchronization primitives to have coherency in the memory shared
    between the CPU and the GPU.
    
    This commit adds a hacky cache flush/invalidation code copied from UMP
    and a msync ioctl to allow cache flush or cache invalidation of a memory
    buffer.
    
    [endlessm/eos-shell#5198]
    
    Signed-off-by: João Paulo Rechi Vita <jprvita@endlessm.com>

---
+++ drivers/gpu/drm/meson/meson_cache_control.c
@@ -0,0 +1,185 @@
+/*
+ * Copyright (C) 2015 Endless Mobile
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * Written by:
+ *     João Paulo Rechi Vita <jprvita@endlessm.com>
+ */
+
+#include <asm/cacheflush.h>
+#include <drm/drm_gem_cma_helper.h>
+#include <drm/meson_drm.h>
+
+#include "meson_priv.h"
+
+#define DBG_MSG(level,args) do {} while(0) /* FIXME: debug properly */
+
+static void level1_cache_flush_all(void)
+{
+	DBG_MSG(4, ("meson_drm_ump_osk_msync(): Flushing the whole L1 cache\n"));
+	__cpuc_flush_kern_all();
+}
+
+/* This is a copy of _ump_osk_msync from drivers/amlogic/gpu/ump/linux/ump_osk_low_level_mem.c
+ * with adapted parameters */
+static void meson_drm_ump_osk_msync(struct drm_gem_cma_object *cma_obj, void *virt, u32 offset, size_t size, enum meson_drm_msync_op op, struct meson_drm_session_data *session_data)
+{
+	struct drm_gem_object *gem_obj;
+	u32 start_p, end_p;
+
+	/* Flush L1 using virtual address, the entire range in one go.
+	 * Only flush if user space process has a valid write mapping on given address. */
+	if ((cma_obj) && (virt != NULL) && (access_ok(VERIFY_WRITE, virt, size))) {
+		__cpuc_flush_dcache_area(virt, size);
+		DBG_MSG(3, ("meson_drm_ump_osk_msync(): Flushing CPU L1 Cache. CPU address: %x, size: %x\n", virt, size));
+	} else {
+		if (session_data) {
+			if (op == MESON_DRM_MSYNC_FLUSH_L1) {
+				DBG_MSG(4, ("meson_drm_ump_osk_msync(): Pending %d L1 cache flushes\n", session_data->has_pending_level1_cache_flush));
+				session_data->has_pending_level1_cache_flush = 0;
+				level1_cache_flush_all();
+				return;
+			} else {
+				if (session_data->cache_operations_ongoing) { /* This is set in cache_operations_control(start) */
+					session_data->has_pending_level1_cache_flush++;
+					DBG_MSG(4, ("meson_drm_ump_osk_msync(): Defering L1 cache flush (%d pending)\n" session_data->has_pending_level1_cache_flush));
+				} else {
+					/* Flushing the L1 cache for each switch_user() if ump_cache_operations_control(START) is not called */
+					level1_cache_flush_all();
+				}
+			}
+		} else {
+			DBG_MSG(4, ("Unkown state %s %d\n", __FUNCTION__, __LINE__));
+			level1_cache_flush_all();
+		}
+	}
+
+	if (!cma_obj)
+		return;
+	gem_obj = &cma_obj->base;
+
+	DBG_MSG(3, ("meson_drm_ump_osk_msync(): Flushing CPU L2 Cache\n"));
+
+	/* Flush L2 using physical addresses
+	 * Our allocations are always contiguous (GEM CMA), so we have only one mem block */
+
+	if (offset >= gem_obj->size) {
+		offset -= gem_obj->size;
+		return;
+	}
+
+	if (offset) {
+		start_p = (u32)cma_obj->paddr + offset;
+		/* We'll zero the offset later, after using it to calculate end_p. */
+	} else {
+		start_p = (u32)cma_obj->paddr;
+	}
+
+	if (size < gem_obj->size - offset) {
+		end_p = start_p + size;
+		size = 0;
+	} else {
+		if (offset) {
+			end_p = start_p + (gem_obj->size - offset);
+			size -= gem_obj->size - offset;
+			offset = 0;
+		} else {
+			end_p = start_p + gem_obj->size;
+			size -= gem_obj->size;
+		}
+	}
+
+	switch (op) {
+	case MESON_DRM_MSYNC_CLEAN:
+		outer_clean_range(start_p, end_p);
+		break;
+	case MESON_DRM_MSYNC_CLEAN_AND_INVALIDATE:
+		outer_flush_range(start_p, end_p);
+		break;
+	case MESON_DRM_MSYNC_INVALIDATE:
+		outer_inv_range(start_p, end_p);
+		break;
+	default:
+		break;
+	}
+
+	return;
+}
+
+/* this code was heavily inspired by _ump_ukk_msync() in
+ * drivers/amlogic/gpu/ump/common/ump_kernel_api.c */
+int meson_ioctl_msync(struct drm_device *dev, void *data, struct drm_file *file)
+{
+	struct drm_gem_object *gem_obj;
+	struct drm_gem_cma_object *cma_obj;
+	struct drm_meson_msync *args = data;
+	struct meson_drm_session_data *session_data = file->driver_priv;
+
+	void *virtual = NULL;
+	u32 size = 0;
+	u32 offset = 0;
+
+	if (!args || !session_data)
+		return -EINVAL;
+
+	gem_obj = drm_gem_object_lookup(dev, file, args->handle);
+	if (NULL == gem_obj) {
+		DBG_MSG(1, ("meson_ioctl_msync(): %02u Failed to look up mapping\n", args->handle));
+		return -EFAULT;
+	}
+
+	cma_obj = to_drm_gem_cma_obj(gem_obj);
+	if (NULL == cma_obj) {
+		DBG_MSG(1, ("meson_ioctl_msync(): %02u Failed to get gem_cma_obj containing gem_obj\n", args->handle));
+		return -EFAULT;
+	}
+
+	/* Returns the cache settings back to Userspace */
+	args->is_cached = dma_get_attr(DMA_ATTR_NON_CONSISTENT, &cma_obj->dma_attrs);
+
+	DBG_MSG(3, ("meson_ioctl_msync(): %02u cache_enabled %d\n op %d address 0x%08x mapping 0x%08x\n",
+		    args->handle, args->is_cached, args->op, args->address, args->mapping));
+
+	/* Nothing to do in these cases */
+	if ((MESON_DRM_MSYNC_READOUT_CACHE_ENABLED == args->op) || (!args->is_cached))
+		return 0;
+
+	if (args->address) {
+		virtual = (void *)((u32)args->address);
+		offset = (u32)((args->address) - (args->mapping));
+	} else {
+		/* Flush entire mapping when no address is specified. */
+		virtual = args->mapping;
+	}
+
+	if (args->size) {
+		size = args->size;
+	} else {
+		/* Flush entire mapping when no size is specified. */
+		size = gem_obj->size - offset;
+	}
+
+	if ((offset + size) > gem_obj->size) {
+		DBG_MSG(1, ("meson_ioctl_msync(): %02u Trying to flush more than the entire allocation: offset %u + size %u > %u\n", args->handle, offset, size, gem_obj->size));
+		return -EINVAL;
+	}
+
+	/* No need to lock here since session_data=NULL */
+	meson_drm_ump_osk_msync(cma_obj, virtual, offset, gem_obj->size, args->op, NULL);
+
+	return 0;
+}
