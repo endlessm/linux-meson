55efc2d80ccc62f15335799848f35e65b10d1b8d
Daniel Drake <drake@endlessm.com>
2015-10-21 14:36:13 -0600

    Revert "USB: Trigger USB FIQ bridge IRQ only when necessary"
    This reverts commit 9665ab32a18260d9c6f8c62dd74448631d14f5e2.
    USB/FIQ is unstable, revert until known issues are solved.

--- drivers/amlogic/usb/dwc_otg/310/dwc_otg_fiq_fsm.c
+++ drivers/amlogic/usb/dwc_otg/310/dwc_otg_fiq_fsm.c
@@ -1232,8 +1232,7 @@ void notrace dwc_otg_fiq_fsm(struct fiq_state *state, int num_channels)
 
 	//}
 	state->fiq_done++;
-	if (kick_irq)
-		WRITE_CBUS_REG(ISA_TIMERD, 1);
+	WRITE_CBUS_REG(ISA_TIMERD, 1);
 	//mb();
 }
 
